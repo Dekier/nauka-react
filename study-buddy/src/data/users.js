b94bf129e3fbbb75bb0776a7fd48cc893363d73d
Marcin Dekier <marcin.dekier@appunite.com>
2022-02-11 11:14:14 +0100

    add users list componets

---
+++ study-buddy/src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'localStorage',
+    attendance: '10%',
+    average: '2.3',
+  },
+  {
+    name: 'Marcin',
+    attendance: '12%',
+    average: '8.3',
+  },
+  {
+    name: 'Ela',
+    attendance: '23%',
+    average: '5.3',
+  },
+  {
+    name: 'Ziomek',
+    attendance: '1%',
+    average: '3.3',
+  },
+  {
+    name: 'Piotr',
+    attendance: '24%',
+    average: '2.31',
+  },
+];
